8b7542e72f82fa11e49f9221a4e35649cea89ed8
Alexis <alexis.oney@pelostud.io>
2021-03-27 14:01:55 +0100

    Add generate-component

---
+++ .generate-component/component-templates.js
@@ -0,0 +1,36 @@
+// Inspired by
+// https://levelup.gitconnected.com/how-to-generate-react-components-from-your-terminal-a27741a5b862
+
+exports.component = (name) => `import styles from './${name}.module.scss';
+
+export type Props = {}
+
+const ${name} = ({}: Props) => {
+  return <div>Hello World!</div>;
+};
+
+export default ${name};
+`;
+
+exports.story = (name) => `import {Meta, Story} from '@storybook/react/types-6-0';
+import React from 'react';
+
+import ${name}, {Props} from './${name}';
+
+export default {
+  title: 'Components/${name}',
+  component: ${name},
+} as Meta;
+
+const Template: Story<Props> = (args) => <${name} {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
+`;
+
+exports.styles = () => `@use '@/styles/utils' as *;
+`;
+
+exports.barrel = (name) => `import ${name} from './${name}';
+export default ${name};
+`;
---
+++ .generate-component/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// Inspired by
+// https://levelup.gitconnected.com/how-to-generate-react-components-from-your-terminal-a27741a5b862
+
+const fs = require('fs');
+const {component, story, styles, barrel} = require('./component-templates.js');
+
+// grab component name from terminal argument
+const [name] = process.argv.slice(2);
+if (!name) throw new Error('You must include a component name.');
+
+const componentsDir = './src/components/';
+
+if (!fs.existsSync(componentsDir)) fs.mkdirSync(componentsDir);
+
+const dir = `./src/components/${name}/`;
+
+// throw an error if the file already exists
+if (fs.existsSync(dir)) throw new Error('A component with that name already exists.');
+
+// create the folder
+fs.mkdirSync(dir);
+
+function writeFileErrorHandler(err) {
+  if (err) throw err;
+}
+
+// component.tsx
+fs.writeFile(`${dir}/${name}.tsx`, component(name), writeFileErrorHandler);
+// component.module.scss
+fs.writeFile(`${dir}/${name}.module.scss`, styles(), writeFileErrorHandler);
+// storybook.jsx
+fs.writeFile(`${dir}/${name}.stories.tsx`, story(name), writeFileErrorHandler);
+// index.tsx
+fs.writeFile(`${dir}/index.ts`, barrel(name), writeFileErrorHandler);
